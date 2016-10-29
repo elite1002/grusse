6bb0fce9ce076832781e986c2dd5c709aa7a4321
ninja1002 <bergeron1002@gmail.com>
2016-10-29 11:45:08 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="zh-Hant">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Grusse</title>
+
+	<!-- Bootstrap Core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom Fonts -->
+	<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<!-- Plugin CSS -->
+	<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+	<!-- My CSS -->
+	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!-- Theme CSS -->
+	<link href="css/creative.css" rel="stylesheet">
+
+	<!-- Google Fonts -->
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+</head>
+
+
+<body id="page-top"> 
+	<!--navbar -->
+	<nav id = "mainNav" class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">	    
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<!--左側按鈕-->
+				<ul class="nav navbar-nav ">
+					<!--那些按鈕     -->
+					<li><a style = "margin-right: 20px; " class="navbar-brand page-scroll" href="#page-top"> Grusse </a></li>
+					<li><a class="navbar-brand page-scroll" href="#"> Login </a> </li>
+					<li><a href="register.php"> Register </a></li>
+				</ul>
+				<!--
+				
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Key Words">
+					</div>
+					<button type="submit" class="btn btn-default"> Search </button>
+				</form>
+			-->
+
+			<!--在最右邊-->    <!-- .nav-justified 平分--> 
+			<ul class="nav navbar-right navbar-form nav-pills">
+				<li><a class="page-scroll" href="#news"> News</a></li>
+				<li><a class="page-scroll" href="#categories">Categories</a></li>
+				<li><a class="page-scroll" href="#contact">Contact</a></li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
+
+<!--Transparent--> <!--jumbotron-->
+<header >
+	<div class="row">
+		<div class="col-lg-6 ">
+			<div>
+				<p></p>
+			</div>
+		</div>
+
+		<div class="col-lg-offset-1 col-lg-4">
+			<div class="row main">
+				<div class="main-login main-center">
+				<h1 class="title">Log In</h1>
+				<hr>
+					<form class="form-horizontal" method="post" action="#">
+
+						<div class="form-group">
+							<label for="name" class="cols-sm-2 control-label">Your Name</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="name" id="name"  placeholder="Enter your Name"/>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="email" class="cols-sm-2 control-label">Your Email</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="email" id="email"  placeholder="Enter your Email"/>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="username" class="cols-sm-2 control-label">Username</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="password" class="cols-sm-2 control-label">Password</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+									<input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+									<input type="password" class="form-control" name="confirm" id="confirm"  placeholder="Confirm your Password"/>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group ">
+							<button type="button" class="btn btn-primary btn-lg btn-block login-button">Register</button>
+							<button type="button" class="btn btn-primary btn-lg btn-block login-button">Log In</button>
+						</div>
+						
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--copy-->
+	
+
+</header>
+
+<!--紅色框框-->
+<section class="bg-primary" id="news">
+	<div class ="container">
+		<div class="row little_transparent">
+			<div class="col-lg-8 col-lg-offset-2 text-center">
+				<h1 class="section-heading">Do you like programming?</h1>
+				<hr class="light">
+			</div>
+			<div class="col-lg-8 col-lg-offset-2">
+				<p>
+					"Everyone in this country should learn how to program a computer...<br>Because it teaches you how to think."
+					<footer> Steve Jobs</footer>
+				</p>
+			</blockquote>
+		</div>
+		<div class="col-lg-8 col-lg-offset-2">
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nKIu9yen5nc"></iframe>
+			</div>
+		</div>
+
+	</div>
+</div>
+</section>
+
+
+<!--two sayings-->
+<section class="bg-dark" id="popular_quotes">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 text-center"> 
+				<h1 class="section-heading">Quotes of the day</h1>
+				<hr class="dark">
+			</div>
+			<!--Green one-->
+			<div class="col-lg-6 little_transparent "> 
+
+				<p>
+					"I grabbed onto the wall to support my weight and moved my cast to one side while my father rolled a football to my left leg. I am naturally right-footed, so this was my weaker foot. After a long day in the mine, he would stand there for hours rolling me the ball so I could pass it back to him.”
+					<footer> - David Villa</footer>
+				</p>
+			</div>
+			<!--Blue one-->
+			<div class="col-lg-6 little_transparent" >
+				<p class="lead">
+					“When I was finally allowed out of bed, I still had to be in the cast for four more months, but one of the first things I did was hop out to the yard in front of our house with my father. It was there that we started my football dream together. </p>
+					<footer> - David Villa</footer>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- 3X圖片 -->
+<section id="categories">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 text-center">
+				<h1 class="section-heading"> Categories </h1>
+				<hr class="dark">
+			</div>
+
+			<div class="col-lg-5">
+				<a href="https://www.instagram.com/sr4oficial/" class="thumbnail"> 
+					<img src="img/ramos.jpg" alt="messi" class="img-responsive img-rounded" style="max-height: 280px">     		
+				</a>
+				<button class="btn btn-danger btn-block"> Work </button>
+			</div>
+
+			<div class=" col-lg-3">
+				<a href="https://www.instagram.com/davidvilla/"  class="thumbnail">
+					<img src="img/villa.jpg" alt="Villa!" class="img-responsive img-rounded" style="max-height: 280px">
+				</a>
+				<button type="button" class="btn btn-danger btn-block"> Love </button>
+			</div>
+
+			<div class="col-lg-4">
+				<a href="https://www.instagram.com/ikercasillasoficial/" class="thumbnail">
+					<img src="img/casillas.jpg" alt="Love you iker" class="img-responsive img-rounded" style="max-height: 280px">	  		
+				</a>
+				<button class="btn btn-block btn-danger"> Life </button>
+			</div>
+		</div>
+		<div class="dropup btn-defaul btn" style="margin-top: 10px">
+			<button  class="dropdown-toggle btn btn-default " data-toggle="dropdown"> SEE MORE </button>
+			<ul class="dropdown-menu" role="menu">
+				<li><a class="btn" href="#">Life</a></li>
+				<li><a class="btn" href="#">Love</a></li>
+				<li><a class="btn" href="#">Study</a></li>
+				<!--Divider-->
+				<li class="divider"></li>
+				<li><a class="btn" href="#">Others</a></li>
+			</ul>
+		</div>
+
+	</div>
+</section>
+
+
+<!--Contact US-->
+<section class="bg-primary" id="contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 text-center"> 
+				<h1 class="section-heading"> About Us </h1>
+				<hr class="light">
+				<p>
+					Grusse<br>
+					Web Developement.<br>
+				</p>
+			</div>
+			<div class="col-lg-4 col-lg-offset-2 text-center">
+				<i class="fa fa-phone fa-3x sr-contact"></i>
+				<p>626-224-5973</p>
+			</div>
+			<div class="col-lg-4 text-center">
+				<i class="fa fa-envelope-o fa-3x sr-contact"></i>
+				<p> tcheng6@ncsu.edu </a></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 text-center">  </div>
+		</div>
+	</div>
+</section>
+
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+<!-- Theme JavaScript -->
+<script src="js/creative.min.js"></script>
+</body>
+</html>
