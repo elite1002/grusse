c645ad7da0ae39e32356de106a5676814cc07878
ninja1002 <bergeron1002@gmail.com>
2016-10-29 13:22:15 -0300

    navbar update

--- index.php
+++ index.php
@@ -20,158 +20,145 @@
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	<!-- Plugin CSS -->
-	<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css" >
 	<!-- My CSS -->
 	<link rel="stylesheet" type="text/css" href="css/mystyle.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<!-- Theme CSS -->
-	<link href="css/creative.css" rel="stylesheet">
-
-	<!-- Google Fonts -->
-
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-
+	<link rel="stylesheet" href="css/creative.css" >
 </head>
 
 
 <body id="page-top"> 
 	<!--navbar -->
-	<nav id = "mainNav" class="navbar navbar-default navbar-fixed-top">
-		<div class="container-fluid">	    
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<!--左側按鈕-->
-				<ul class="nav navbar-nav ">
-					<!--那些按鈕     -->
-					<li><a style = "margin-right: 20px; " class="navbar-brand page-scroll" href="#page-top"> Grusse </a></li>
-					<li><a class="navbar-brand page-scroll" href="#"> Login </a> </li>
-					<li><a href="register.php"> Register </a></li>
-				</ul>
-				<!--
-				
-				<form class="navbar-form navbar-left" role="search">
-					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Key Words">
+	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a style = "margin-right: 20px; " class="navbar-brand page-scroll" href="#page-top"> Grusse </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a class="page-scroll" href="#news"> News</a></li>
+                    <li><a class="page-scroll" href="#categories">Categories</a></li>
+                    <li><a class="page-scroll" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+	<header >
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="row main">
+					<div class="main-login main-center">
+						<h2> Look at me </h2>
 					</div>
-					<button type="submit" class="btn btn-default"> Search </button>
-				</form>
-			-->
-
-			<!--在最右邊-->    <!-- .nav-justified 平分--> 
-			<ul class="nav navbar-right navbar-form nav-pills">
-				<li><a class="page-scroll" href="#news"> News</a></li>
-				<li><a class="page-scroll" href="#categories">Categories</a></li>
-				<li><a class="page-scroll" href="#contact">Contact</a></li>
-			</ul>
-		</div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
-</nav>
-
-<!--Transparent--> <!--jumbotron-->
-<header >
-	<div class="row">
-		<div class="col-lg-6 ">
-			<div>
-				<p></p>
+				</div>
 			</div>
-		</div>
 
-		<div class="col-lg-offset-1 col-lg-4">
-			<div class="row main">
-				<div class="main-login main-center">
-				<h1 class="title">Log In</h1>
-				<hr>
-					<form class="form-horizontal" method="post" action="#">
+			<div class="col-lg-offset-1 col-lg-4">
+				<div class="row main">
+					<div class="main-login main-center">
+						<h1 class="title">Join now!</h1>
+						<hr>
+						<form class="form-horizontal" method="post" action="#">
 
-						<div class="form-group">
-							<label for="name" class="cols-sm-2 control-label">Your Name</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="name" id="name"  placeholder="Enter your Name"/>
+							<div class="form-group">
+								<label for="name" class="cols-sm-2 control-label">Your Name</label>
+								<div class="cols-sm-10">
+									<div class="input-group">
+										<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+										<input type="text" class="form-control" name="name" id="name"  placeholder="Enter your Name"/>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<label for="email" class="cols-sm-2 control-label">Your Email</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="email" id="email"  placeholder="Enter your Email"/>
+							<div class="form-group">
+								<label for="email" class="cols-sm-2 control-label">Your Email</label>
+								<div class="cols-sm-10">
+									<div class="input-group">
+										<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+										<input type="text" class="form-control" name="email" id="email"  placeholder="Enter your Email"/>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<label for="username" class="cols-sm-2 control-label">Username</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+							<div class="form-group">
+								<label for="username" class="cols-sm-2 control-label">Username</label>
+								<div class="cols-sm-10">
+									<div class="input-group">
+										<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+										<input type="text" class="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<label for="password" class="cols-sm-2 control-label">Password</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-									<input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+							<div class="form-group">
+								<label for="password" class="cols-sm-2 control-label">Password</label>
+								<div class="cols-sm-10">
+									<div class="input-group">
+										<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+										<input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div class="form-group">
-							<label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-									<input type="password" class="form-control" name="confirm" id="confirm"  placeholder="Confirm your Password"/>
+							<div class="form-group">
+								<label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
+								<div class="cols-sm-10">
+									<div class="input-group">
+										<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+										<input type="password" class="form-control" name="confirm" id="confirm"  placeholder="Confirm your Password"/>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div class="form-group ">
-							<button type="button" class="btn btn-primary btn-lg btn-block login-button">Register</button>
-							<button type="button" class="btn btn-primary btn-lg btn-block login-button">Log In</button>
-						</div>
-						
-					</form>
+							<div class="form-group ">
+								<button type="button" class="btn btn-primary btn-lg btn-block login-button">Register</button>
+								<button type="button" class="btn btn-mine btn-lg btn-block login-button">Log In</button>
+							</div>
+
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<!--copy-->
-	
+		<!--copy-->
 
-</header>
 
-<!--紅色框框-->
-<section class="bg-primary" id="news">
-	<div class ="container">
-		<div class="row little_transparent">
-			<div class="col-lg-8 col-lg-offset-2 text-center">
-				<h1 class="section-heading">Do you like programming?</h1>
-				<hr class="light">
+	</header>
+
+	<!--紅色框框-->
+	<section class="bg-primary" id="news">
+		<div class ="container">
+			<div class="row little_transparent">
+				<div class="col-lg-8 col-lg-offset-2 text-center">
+					<h1 class="section-heading">Do you like programming?</h1>
+					<hr class="light">
+				</div>
+				<div class="col-lg-8 col-lg-offset-2">
+					<p>
+						"Everyone in this country should learn how to program a computer...<br>Because it teaches you how to think."
+						<footer> Steve Jobs</footer>
+					</p>
+				</blockquote>
 			</div>
 			<div class="col-lg-8 col-lg-offset-2">
-				<p>
-					"Everyone in this country should learn how to program a computer...<br>Because it teaches you how to think."
-					<footer> Steve Jobs</footer>
-				</p>
-			</blockquote>
-		</div>
-		<div class="col-lg-8 col-lg-offset-2">
-			<div class="embed-responsive embed-responsive-16by9">
-				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nKIu9yen5nc"></iframe>
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nKIu9yen5nc"></iframe>
+				</div>
 			</div>
-		</div>
 
+		</div>
 	</div>
-</div>
 </section>
 
 
