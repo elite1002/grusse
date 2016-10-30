11641908543d8ac3dd96081a164822c37978d56a
ninja1002 <bergeron1002@gmail.com>
2016-10-30 00:21:29 -0300

    new interface

---
+++ userhome.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="zh-Hant">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Grusse</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="/assets/css/main.css" >
+    <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css" >
+
+    <!-- Custom Fonts -->
+    <link href="/vendor/font-awesome/scss/font-awesome.scss" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="/vendor/magnific-popup/magnific-popup.css" >
+    <!-- My CSS -->
+    <link rel="stylesheet" type="text/css" href="/css/mystyle.css">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="/css/creative.css" >
+    
+</head>
+
+
+<body id="page-top"> 
+    <!--navbar -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a style = "margin-right: 20px; " class="navbar-brand page-scroll" href="#page-top"> Grusse </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                   <li><a style ="margin-left: 40px" class="page-scroll" href="#new"><span class="glyphicon glyphicon-bell"></span> </a> </li>
+                   <li><a style ="margin-left: 40px" class="page-scroll" href="#mailbox"><span class="glyphicon glyphicon-envelope"></span> </a> </li>
+                   <li><a style ="margin-left: 40px" class="page-scroll" href="#favorite"><span class="glyphicon glyphicon-star"></span></a></li>
+                   <li><a style ="margin-left: 40px" class="page-scroll" href="#contact"><span class="glyphicon glyphicon-user"></span></a></li>
+               </ul>
+               <ul class="nav navbar-nav navbar-right">
+                   <button class="btn btn-mine" style="margin-top: 8px; margin-right: 40px;"> <a href="#new"><span style=";" class="glyphicon glyphicon-pencil"> Compose </span> </a> </button>
+               </ul>
+           </div>
+           <!-- /.navbar-collapse -->
+       </div>
+       <!-- /.container-fluid -->
+   </nav>
+   <header class="bg-dark" style="background-image:none;">
+    <div class="header-content">
+        <div class="header-content-inner">
+            <h2 class="homeHeading">Welcome back, Username</h2>
+            <hr class="dark">
+            <span class="avatar"><img class="img-rounded" src="/img/avatar.jpg" alt="" /></span>
+            <p> 
+                <br>
+                Postcards Sent: <br>
+                Postcards Received: <br>
+                Hearts Received: <br>
+            </p>
+        </div>
+    </div>
+</header>
+<section class="bg-primary thumbnails" id="mailbox">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6" text-center>
+               <h1 class="section-heading">New Cards</h1>
+               <hr class="light">
+
+               <a href="/img/fulls/01.jpg">
+                <img src="img/thumbs/01.jpg" alt="" />
+                <h4 style="color: white;">alkfjhanflksjhak</h4>
+                </a>
+            <h1> from German. </h1>
+            </div>
+    </div>
+</div>
+
+</section>
+
+
+<!-- jQuery -->
+<script src="/vendor/jquery/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="/vendor/scrollreveal/scrollreveal.min.js"></script>
+<script src="/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<!-- Theme JavaScript -->
+<script src="/js/creative.min.js"></script>
+
+<!--This Page only-->
+<script src="/assets/js/jquery.poptrox.min.js"></script>
+<script src="/assets/js/skel.min.js"></script>
+<script src="/assets/js/main.js"></script>
+
+</body>
+</html>
